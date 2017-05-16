41196db15f3c02a1ba5a44da2597fad5f6a94ae6
carloschristiansalazar <ccsalazar@cpp.edu>
2017-05-15 23:48:02 -0700

    Add files via upload

--- accounts/views.py
+++ accounts/views.py
@@ -1,26 +1,61 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
+from django.contrib.auth import authenticate, login
+from django.contrib.auth import logout
+from django.template import loader 
+from django.shortcuts import render, get_object_or_404
+from .forms import UserForm
 
-from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
 
-# Create your views here.
-def home(request):
-    numbers = [1,2,3,4,5]
-    name = 'Kendy Nguyen'
+def index(request):
+    template = loader.get_template('portfolio/index.html')
+    return render(request, 'portfolio/index.html')
 
-    args = {'myName': name, 'numbers': numbers}
-    return render(request, 'accounts/home.html', args)
+
+
+
+
+
+
+def logout_user(request):
+    logout(request)
+    form = UserForm(request.POST or None)
+    context = {
+        "form": form,
+    }
+    return render(request, 'portfolio/login.html', context)
+
+
+def login_user(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return render(request, 'portfolio/index.html')
+                #if active user do stuff. import stock data etc
+            else:
+                return render(request, 'portfolio/login.html', {'error_message': 'Your account has been disabled'})
+        else:
+            return render(request, 'portfolio/login.html', {'error_message': 'Invalid login'})
+    return render(request, 'portfolio/login.html')
+
 
 def register(request):
-    if request.method =='POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            form.save()
-            return redirect('/accounts')
+    form = UserForm(request.POST or None)
+    if form.is_valid():
+        user = form.save(commit=False)
+        username = form.cleaned_data['username']
+        password = form.cleaned_data['password']
+        user.set_password(password)
+        user.save()
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                login(request, user)
+                return render(request, 'portfolio/index.html')
+    context = {
+        "form": form,
+    }
+    return render(request, 'portfolio/register.html', context)
 
-    else:
-        form = UserCreationForm()
-
-        args ={'form': form}
-        return render(request, 'accounts/reg_form.html', args)
